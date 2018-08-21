9871bbfea2416880243074cc2537a8c3eeaf6518
Dustin Popp <dustinpopp@ibm.com>
2018-08-21 11:30:20 -0500

    use functional style over looping

--- src/plugins/validation/2and3/semantic-validators/parameters-ibm.js
+++ src/plugins/validation/2and3/semantic-validators/parameters-ibm.js
@@ -56,15 +56,8 @@ export function validate({ jsSpec, isOAS3 }, config) {
 
       let isParameter = obj.in // the `in` property is required by OpenAPI for parameters - this should be true (unless obj is a ref)
       let isHeaderParameter = (obj.in && obj.in.toLowerCase() === "header") // header params need not be snake_case
-      let isSnakecase = true
-      if (obj.name) {
-        // Relax snakecase check to allow names with "."
-        obj.name.split(".").forEach(segment => {
-          if (!checkSnakecase(segment)) {
-            isSnakecase = false
-          }
-        })
-      }
+      // Relax snakecase check to allow names with "."
+      let isSnakecase = !(obj.name) || obj.name.split(".").map(s => checkSnakecase(s)).every(v => v)
 
       // if the parameter is defined by a ref, no need to check the ref path for snake_case
       if (isParameter && !isHeaderParameter && !isRef && !isSnakecase) {
