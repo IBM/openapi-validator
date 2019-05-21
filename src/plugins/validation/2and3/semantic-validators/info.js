86d1c5f40f411235e0c79878f61ffffe97bacea4
System Administrator <root@samers-mbp.austin.ibm.com>
2019-05-21 14:35:01 -0500

    chore: fix failing tests

---
+++ src/plugins/validation/2and3/semantic-validators/info.js
@@ -0,0 +1,28 @@
+// Assertation 1:
+// check if info exists
+
+// Assertation 2:
+// making sure that the required version and title are defined properly
+module.exports.validate = function({ jsSpec }) {
+  const errors = [];
+  const warnings = [];
+  if (!jsSpec.info) {
+    errors.push({
+      path: ['info'],
+      message: 'Missing info object for api defintion'
+    });
+  }
+  //Assertion 2
+  if (
+    jsSpec.info &&
+    (typeof jsSpec.info.title != 'string' ||
+      typeof jsSpec.info.version != 'string')
+  ) {
+    errors.push({
+      path: ['info', 'title'],
+      message:
+        'Info object missing proper definitions for title or version properties'
+    });
+  }
+  return { errors: errors, warnings: warnings };
+};
---
+++ test/plugins/validation/2and3/info.js
@@ -0,0 +1,41 @@
+const config = require('../../../../src/.defaultsForValidator').defaults.shared;
+const expect = require('expect');
+const {
+  validate
+} = require('../../../../src/plugins/validation/2and3/semantic-validators/info');
+
+describe('validation plugin - semantic - error handling for incorrect formatting', () => {
+  describe('there should be errors when the defintion is missing neccesrry fields', function() {
+    //this is for openapi object
+    it('should return an error when a parameter does not have info', () => {
+      const spec = {
+        Openapi: '3.0.0'
+      };
+
+      const res = validate({ jsSpec: spec }, config);
+      //expect(res.warnings.length).toEqual(0);
+      expect(res.errors.length).toEqual(1);
+      expect(res.errors[0].path).toEqual(['info']);
+      expect(res.errors[0].message).toEqual(
+        'Missing info object for api defintion'
+      );
+    });
+    it('should return an error when a title and version are not strings', () => {
+      const spec = {
+        Openapi: '3.0.0',
+        info: {
+          title: 32,
+          version: 1
+        }
+      };
+
+      const res = validate({ jsSpec: spec }, config);
+      //expect(res.warnings.length).toEqual(0);
+      expect(res.errors.length).toEqual(1);
+      expect(res.errors[0].path).toEqual(['info', 'title']);
+      expect(res.errors[0].message).toEqual(
+        'Info object missing proper definitions for title or version properties'
+      );
+    });
+  });
+});
