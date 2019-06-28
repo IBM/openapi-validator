d61d1af72008af2939a47345cdaf0bfb5d13aa61
Dustin Popp <dpopp07@gmail.com>
2019-06-28 13:50:36 -0500

    fix: response schema support for oneOf, anyOf, and allOf refs
    Check for inline schema definitions in combined schemas

--- test/plugins/validation/2and3/responses.js
+++ test/plugins/validation/2and3/responses.js
@@ -161,6 +161,116 @@ describe('validation plugin - semantic - responses', function() {
         expect(res.errors.length).toEqual(0);
       });
 
+      it('should not complain for a valid response in oneOf', function() {
+        const spec = {
+          paths: {
+            '/stuff': {
+              get: {
+                summary: 'list stuff',
+                operationId: 'listStuff',
+                responses: {
+                  200: {
+                    description: 'successful operation',
+                    content: {
+                      'application/json': {
+                        schema: {
+                          oneOf: [
+                            {
+                              $ref:
+                                '#/components/schemas/ListStuffResponseModel'
+                            },
+                            {
+                              $ref: '#/components/schemas/ListStuffSecondModel'
+                            }
+                          ]
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        };
+
+        const res = validate({ jsSpec: spec, isOAS3: true }, config);
+        expect(res.warnings.length).toEqual(0);
+        expect(res.errors.length).toEqual(0);
+      });
+
+      it('should not complain for a valid response in allOf', function() {
+        const spec = {
+          paths: {
+            '/stuff': {
+              get: {
+                summary: 'list stuff',
+                operationId: 'listStuff',
+                responses: {
+                  200: {
+                    description: 'successful operation',
+                    content: {
+                      'application/json': {
+                        schema: {
+                          allOf: [
+                            {
+                              $ref:
+                                '#/components/schemas/ListStuffResponseModel'
+                            },
+                            {
+                              $ref: '#/components/schemas/ListStuffSecondModel'
+                            }
+                          ]
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        };
+
+        const res = validate({ jsSpec: spec, isOAS3: true }, config);
+        expect(res.warnings.length).toEqual(0);
+        expect(res.errors.length).toEqual(0);
+      });
+
+      it('should not complain for a valid response in anyOf', function() {
+        const spec = {
+          paths: {
+            '/stuff': {
+              get: {
+                summary: 'list stuff',
+                operationId: 'listStuff',
+                responses: {
+                  200: {
+                    description: 'successful operation',
+                    content: {
+                      'application/json': {
+                        schema: {
+                          anyOf: [
+                            {
+                              $ref:
+                                '#/components/schemas/ListStuffResponseModel'
+                            },
+                            {
+                              $ref: '#/components/schemas/ListStuffSecondModel'
+                            }
+                          ]
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        };
+
+        const res = validate({ jsSpec: spec, isOAS3: true }, config);
+        expect(res.warnings.length).toEqual(0);
+        expect(res.errors.length).toEqual(0);
+      });
       it('should complain about an inline schema', function() {
         const spec = {
           paths: {
@@ -201,6 +311,159 @@ describe('validation plugin - semantic - responses', function() {
           'content',
           'application/json',
           'schema'
+        ]);
+        expect(res.warnings[0].message).toEqual(
+          'Response schemas should be defined with a named ref.'
+        );
+        expect(res.errors.length).toEqual(0);
+      });
+
+      it('should complain about an inline schema when using oneOf', function() {
+        const spec = {
+          paths: {
+            '/stuff': {
+              get: {
+                summary: 'list stuff',
+                operationId: 'listStuff',
+                responses: {
+                  200: {
+                    description: 'successful operation',
+                    content: {
+                      'application/json': {
+                        schema: {
+                          oneOf: [
+                            {
+                              type: 'object'
+                            },
+                            {
+                              $ref: 'ref1'
+                            }
+                          ]
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        };
+
+        const res = validate({ jsSpec: spec, isOAS3: true }, config);
+        expect(res.warnings.length).toEqual(1);
+        expect(res.warnings[0].path).toEqual([
+          'paths',
+          '/stuff',
+          'get',
+          'responses',
+          '200',
+          'content',
+          'application/json',
+          'schema',
+          'oneOf',
+          0
+        ]);
+        expect(res.warnings[0].message).toEqual(
+          'Response schemas should be defined with a named ref.'
+        );
+        expect(res.errors.length).toEqual(0);
+      });
+
+      it('should complain about an inline schema when using allOf', function() {
+        const spec = {
+          paths: {
+            '/stuff': {
+              get: {
+                summary: 'list stuff',
+                operationId: 'listStuff',
+                responses: {
+                  200: {
+                    description: 'successful operation',
+                    content: {
+                      'application/json': {
+                        schema: {
+                          allOf: [
+                            {
+                              type: 'object'
+                            },
+                            {
+                              $ref: 'ref1'
+                            }
+                          ]
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        };
+
+        const res = validate({ jsSpec: spec, isOAS3: true }, config);
+        expect(res.warnings.length).toEqual(1);
+        expect(res.warnings[0].path).toEqual([
+          'paths',
+          '/stuff',
+          'get',
+          'responses',
+          '200',
+          'content',
+          'application/json',
+          'schema',
+          'allOf',
+          0
+        ]);
+        expect(res.warnings[0].message).toEqual(
+          'Response schemas should be defined with a named ref.'
+        );
+        expect(res.errors.length).toEqual(0);
+      });
+
+      it('should complain about an inline schema when using anyOf', function() {
+        const spec = {
+          paths: {
+            '/stuff': {
+              get: {
+                summary: 'list stuff',
+                operationId: 'listStuff',
+                responses: {
+                  200: {
+                    description: 'successful operation',
+                    content: {
+                      'application/json': {
+                        schema: {
+                          anyOf: [
+                            {
+                              type: 'object'
+                            },
+                            {
+                              $ref: 'ref1'
+                            }
+                          ]
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        };
+
+        const res = validate({ jsSpec: spec, isOAS3: true }, config);
+        expect(res.warnings.length).toEqual(1);
+        expect(res.warnings[0].path).toEqual([
+          'paths',
+          '/stuff',
+          'get',
+          'responses',
+          '200',
+          'content',
+          'application/json',
+          'schema',
+          'anyOf',
+          0
         ]);
         expect(res.warnings[0].message).toEqual(
           'Response schemas should be defined with a named ref.'
