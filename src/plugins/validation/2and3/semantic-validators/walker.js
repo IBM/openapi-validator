56555fef464223d76ca06833b8cb220614e99179
Samer Jaser <samerjaser@samers-mbp.austin.ibm.com>
2019-07-17 12:41:20 -0500

    adjustment to walker

--- src/plugins/validation/2and3/semantic-validators/walker.js
+++ src/plugins/validation/2and3/semantic-validators/walker.js
@@ -15,9 +15,8 @@ const match = require('matcher');
 const walk = require('../../../utils/walk');
 
 module.exports.validate = function({ jsSpec, isOAS3 }, config) {
-  const result = {};
-  result.error = [];
-  result.warning = [];
+  const errors = [];
+  const warnings = [];
 
   config = config.walker;
 
@@ -45,7 +44,7 @@ module.exports.validate = function({ jsSpec, isOAS3 }, config) {
     ///// "type" should always have a string-type value, everywhere.
     if (obj.type && allowedParents.indexOf(path[path.length - 1]) === -1) {
       if (typeof obj.type !== 'string') {
-        result.error.push({
+        errors.push({
           path: [...path, 'type'],
           message: '"type" should be a string'
         });
@@ -56,7 +55,7 @@ module.exports.validate = function({ jsSpec, isOAS3 }, config) {
 
     if (obj.maximum && obj.minimum) {
       if (greater(obj.minimum, obj.maximum)) {
-        result.error.push({
+        errors.push({
           path: path.concat(['minimum']),
           message: 'Minimum cannot be more than maximum'
         });
@@ -65,7 +64,7 @@ module.exports.validate = function({ jsSpec, isOAS3 }, config) {
 
     if (obj.maxProperties && obj.minProperties) {
       if (greater(obj.minProperties, obj.maxProperties)) {
-        result.error.push({
+        errors.push({
           path: path.concat(['minProperties']),
           message: 'minProperties cannot be more than maxProperties'
         });
@@ -74,7 +73,7 @@ module.exports.validate = function({ jsSpec, isOAS3 }, config) {
 
     if (obj.maxLength && obj.minLength) {
       if (greater(obj.minLength, obj.maxLength)) {
-        result.error.push({
+        errors.push({
           path: path.concat(['minLength']),
           message: 'minLength cannot be more than maxLength'
         });
@@ -90,38 +89,45 @@ module.exports.validate = function({ jsSpec, isOAS3 }, config) {
       if (refBlacklist && refBlacklist.length && matches.length) {
         // Assertation 2
         // use the slice(1) to remove the `!` negator from the string
-        const checkStatus = config.incorrect_ref_pattern;
-        if (checkStatus !== 'off') {
-          result[checkStatus].push({
-            path: [...path, '$ref'],
-            message: `${
-              blacklistPayload.location
-            } $refs must follow this format: ${refBlacklist[0].slice(1)}`
-          });
-        }
+        warnings.push({
+          path: [...path, '$ref'],
+          message: `${
+            blacklistPayload.location
+          } $refs must follow this format: ${refBlacklist[0].slice(1)}`
+        });
       }
     }
 
     const keys = Object.keys(obj);
     keys.forEach(k => {
       if (keys.indexOf('$ref') > -1 && k !== '$ref') {
-        const checkStatus = config.$ref_siblings;
-        if (checkStatus !== 'off') {
-          result[checkStatus].push({
-            path: path.concat([k]),
-            message: 'Values alongside a $ref will be ignored.'
-          });
+        switch (config.$ref_siblings) {
+          case 'error':
+            errors.push({
+              path: path.concat([k]),
+              message: 'Values alongside a $ref will be ignored.'
+            });
+            break;
+          case 'warning':
+            warnings.push({
+              path: path.concat([k]),
+              message: 'Values alongside a $ref will be ignored.'
+            });
+            break;
+          default:
+            break;
         }
       }
     });
   });
 
-  return { errors: result.error, warnings: result.warning };
+  return { errors, warnings };
 };
 
 // values are globs!
 const unacceptableRefPatternsS2 = {
   responses: ['!*#/responses*'],
+  links: ['!*#/links*'],
   schema: ['!*#/definitions*'],
   parameters: ['!*#/parameters*']
 };
