36c32f0f9d1031d848d51b33a7f29e2335d24946
Shuyu Chen <shuyuchen@ibm.com>
2019-09-27 16:52:36 -0500

    feat: adds discriminator validator to conform to openAPI 2 and 3 specifications
    The Swagger 2.0 spec says 'The [discriminator] property name used MUST be defined at this schema'.
    The validator currently does not flag violations of this requirement

---
+++ src/plugins/validation/2and3/semantic-validators/discriminator.js
@@ -0,0 +1,74 @@
+// Assertation 1:
+// if discriminator exist inside schema object, it must be of type Object
+
+// Assertion 2:
+// discriminator object must have a field name propertyName
+
+// Assertation 2:
+// propertyName is of type string
+
+// Assertation 3:
+// properties inside a schema object must include propertName from discriminator object
+
+const each = require('lodash/each');
+const has = require('lodash/has');
+
+module.exports.validate = function({ jsSpec }) {
+  const errors = [];
+  const warnings = [];
+
+  const schemas = jsSpec.components.schemas;
+
+  const basePath = ['components', 'schemas'];
+
+  each(schemas, (schema, schemaName) => {
+    if (has(schema, 'discriminator')) {
+      const { discriminator } = schema;
+
+      // If discriminator is not an object, error out and return
+      if (typeof discriminator === 'object') {
+        if (!has(discriminator, 'propertyName')) {
+          errors.push({
+            path: basePath.concat([schemaName, 'discriminator']).join('.'),
+            message:
+              'Discriminator must be of type object with field name propertyName'
+          });
+          return;
+        }
+      } else {
+        errors.push({
+          path: basePath.concat([schemaName, 'discriminator']).join('.'),
+          message: 'Discriminator must be of type object'
+        });
+        return;
+      }
+
+      // If discriminator propertyName is not a string, error out and return
+      const { propertyName } = discriminator;
+      if (typeof propertyName !== 'string') {
+        errors.push({
+          path: basePath
+            .concat([schemaName, 'discriminator', 'propertyName'])
+            .join('.'),
+          message:
+            '`propertName` inside discriminator object must be of type string'
+        });
+        return;
+      }
+
+      // If the schema's property doesn't include propertyName defined in discriminator, error out and return
+      const { properties } = schema;
+      if (!has(properties, propertyName)) {
+        errors.push({
+          path: basePath
+            .concat([schemaName, 'discriminator', 'propertyName'])
+            .join('.'),
+          message:
+            'The discriminator property name used must be defined in this schema'
+        });
+        return;
+      }
+    }
+  });
+  return { errors, warnings };
+};
