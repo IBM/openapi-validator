d172958c5903ddfb23f390ae81833040e29a4d59
Shuyu Chen <shuyuchen@ibm.com>
2020-01-28 15:14:39 -0600

    initial changes for checking operation id name convention

--- src/plugins/validation/2and3/semantic-validators/operation-ids.js
+++ src/plugins/validation/2and3/semantic-validators/operation-ids.js
@@ -1,5 +1,7 @@
 // Assertation 1: Operations must have a unique operationId.
 
+// Assertation 2: OperationId must conform to naming conventions.
+
 const pickBy = require('lodash/pickBy');
 const reduce = require('lodash/reduce');
 const merge = require('lodash/merge');
@@ -30,6 +32,8 @@ module.exports.validate = function({ resolvedSpec }) {
         arr.push(
           merge(
             {
+              pathKey: `${pathKey}`,
+              opKey: `${opKey}`,
               path: `paths.${pathKey}.${opKey}`
             },
             op
@@ -50,15 +54,63 @@ module.exports.validate = function({ resolvedSpec }) {
     return !!prev;
   };
 
+  const operationIdPassedConventionCheck = (opKey, operationId, pathParam) => {
+    // Only consider paths for which
+    // - paths with no path param has a GET and POST path
+    // - paths with path param has a GET, a DELETE, and a POST or PUT or PATCH.
+
+    let checkPassed = true
+
+    if (!pathParam) {
+      // No path param
+      if (opKey === 'get' && !operationId.match(/^list[a-zA-Z0-9_]+/m)) {
+        // operationId for GET starts with "list"
+        checkPassed = false
+      }
+      if (opKey === 'post' && !operationId.match(/^(add|create)[a-zA-Z0-9_]+/m)) {
+        // operationId for POST starts with "create" or "add"
+        checkPassed = false
+      }
+    } else {
+      // There is path param
+      if (opKey === 'get' && !operationId.match(/^get[a-zA-Z0-9_]+/m)) {
+        // operationId for GET starts with "get"
+        checkPassed = false
+      }
+      if (opKey === 'delete' && !operationId.match(/^delete[a-zA-Z0-9_]+/m)) {
+        // operationId for DELETE starts with "delete"
+        checkPassed = false
+      }
+      if (opKey in ['put', 'post', 'patch'] && !operationId.match(/^update[a-zA-Z0-9_]+/m)) {
+        // operationId for POST/PUT/PATCH starts with "update"
+        checkPassed = false
+      }
+    }
+    return checkPassed
+  }
+
   operations.forEach(op => {
     // wrap in an if, since operationIds are not required
     if (op.operationId) {
       const hasBeenSeen = tallyOperationId(op.operationId);
       if (hasBeenSeen) {
+        // Assertation 1: Operations must have a unique operationId.
         errors.push({
           path: op.path + '.operationId',
           message: 'operationIds must be unique'
         });
+      } else {
+        // Assertation 2: OperationId must conform to naming conventions
+        const regex = RegExp(/{[a-zA-Z0-9_-]+\}/m);
+
+        const checkPassed = operationIdPassedConventionCheck(op['opKey'], op.operationId, regex.test(op['pathKey']))
+
+        if (checkPassed === false) {
+          warnings.push({
+            path: op.path + '.operationId',
+            message: `operationIds should follow consistent naming convention`
+          });
+        }
       }
     }
   });
