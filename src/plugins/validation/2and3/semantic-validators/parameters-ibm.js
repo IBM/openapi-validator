600bf8e301937f72868845fc6baeaff97ae999a8
barrett-schonefeld <59850348+barrett-schonefeld@users.noreply.github.com>
2020-02-25 11:27:28 -0600

    feat: list valid types and formats in error messages (#145)
    Changes the way problems with type/format values are handled so that messages are more descriptive about what is actually wrong

--- src/plugins/validation/2and3/semantic-validators/parameters-ibm.js
+++ src/plugins/validation/2and3/semantic-validators/parameters-ibm.js
@@ -8,8 +8,6 @@
 // Header parameters must not define a content-type or an accept-type.
 // http://watson-developer-cloud.github.io/api-guidelines/swagger-coding-style#do-not-explicitly-define-a-content-type-header-parameter
 
-const pick = require('lodash/pick');
-const includes = require('lodash/includes');
 const { checkCase, isParameterObject, walk } = require('../../../utils');
 const MessageCarrier = require('../../../utils/messageCarrier');
 
@@ -107,18 +105,6 @@ module.exports.validate = function({ jsSpec, isOAS3 }, config) {
         }
       }
 
-      const checkStatus = config.invalid_type_format_pair;
-      if (checkStatus !== 'off') {
-        const valid = formatValid(obj, isOAS3);
-        if (!valid) {
-          messages.addMessage(
-            path,
-            'Parameter type+format is not well-defined.',
-            checkStatus
-          );
-        }
-      }
-
       const isParameterRequired = obj.required;
       let isDefaultDefined;
       if (isOAS3) {
@@ -139,46 +125,3 @@ module.exports.validate = function({ jsSpec, isOAS3 }, config) {
 
   return messages;
 };
-
-function formatValid(obj, isOAS3) {
-  // References will be checked when the parameters / definitions / components are scanned.
-  if (obj.$ref || (obj.schema && obj.schema.$ref)) {
-    return true;
-  }
-  const schema = obj.schema || pick(obj, ['type', 'format', 'items']);
-  if (!schema.type) {
-    return false;
-  }
-  switch (schema.type) {
-    case 'integer':
-      return (
-        !schema.format ||
-        includes(['int32', 'int64'], schema.format.toLowerCase())
-      );
-    case 'number':
-      return (
-        !schema.format ||
-        includes(['float', 'double'], schema.format.toLowerCase())
-      );
-    case 'string':
-      return (
-        !schema.format ||
-        includes(
-          ['byte', 'date', 'date-time', 'password'],
-          schema.format.toLowerCase()
-        )
-      );
-    case 'boolean':
-      return schema.format === undefined; // No valid formats for boolean -- should be omitted
-    case 'array':
-      if (!schema.items) {
-        return false;
-      }
-      return formatValid(schema.items);
-    case 'object':
-      return true; // TODO: validate nested schemas
-    case 'file':
-      return !isOAS3 && obj.in === 'formData';
-  }
-  return false;
-}
