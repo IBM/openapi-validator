ea4f09a6e9a1dd65248069ee5b5ccb0307291bf1
Barrett Schonefeld <barrett.schonefeld@ibm.com>
2021-04-26 09:04:30 -0500

    refactor: replace openapi object validation with Spectral oas3-schema validation

--- test/plugins/validation/oas3/openapi.test.js
+++ test/plugins/validation/oas3/openapi.test.js
@@ -17,30 +17,4 @@ describe('validation plugin - semantic - openapi', () => {
       'API definition must have an `openapi` field'
     );
   });
-
-  it('should return an error when an openapi field is not a string', () => {
-    const spec = {
-      openapi: 123
-    };
-
-    const res = validate({ jsSpec: spec });
-    expect(res.errors.length).toEqual(1);
-    expect(res.errors[0].path).toEqual(['openapi']);
-    expect(res.errors[0].message).toEqual(
-      'API definition must have an `openapi` field as type string'
-    );
-  });
-
-  it('should return an error when an openapi does not conform to semantic versioning 2.0.0', () => {
-    const spec = {
-      openapi: 'v1.0.10'
-    };
-
-    const res = validate({ jsSpec: spec });
-    expect(res.errors.length).toEqual(1);
-    expect(res.errors[0].path).toEqual(['openapi']);
-    expect(res.errors[0].message).toEqual(
-      '`openapi` string must follow Semantic Versioning 2.0.0'
-    );
-  });
 });
