f407f88104c0f7dbe2dc6872fa36b1bcc54cf7c6
Dustin Popp <dpopp07@gmail.com>
2022-02-14 15:40:42 -0600

    fix: pin spectral dependencies to avoid configuration bugs
    This updates Spectral dependencies to their latest versions and pins them so that
    they don't change underneath us and impact our users. We will have to manually update
    all Spectral dependencies from now on, which I think is worth it for the time being.
    
    The latest Spectral core release also includes my fixes to the de-duplication logic,
    so I removed our custom function from this code. We can rely on the default Spectral
    behavior now.

--- packages/ruleset/test/utils/test-rule.js
+++ packages/ruleset/test/utils/test-rule.js
@@ -10,15 +10,6 @@ const { Spectral } = require('@stoplight/spectral-core');
 module.exports = async (ruleName, rule, doc) => {
   const spectral = new Spectral();
 
-  // define a custom 'compute fingerprint' function that ensures
-  // validation ids are always unique. otherwise, rules with the same
-  // code and path with overwrite each other
-  let ruleId = 0;
-  spectral._computeFingerprint = () => {
-    ruleId++;
-    return ruleId;
-  };
-
   spectral.setRuleset({
     rules: {
       [ruleName]: rule
