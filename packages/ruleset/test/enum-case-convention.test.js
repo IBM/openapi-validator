ed65fd2d9c5f470efa10605350f02f5837bc4edf
Phil Adams <phil_adams@us.ibm.com>
2022-02-19 11:26:48 -0600

    feat(enum-case-convention): add new enum-case-convention spectral rule, remove old rule
    This commit replaces the old enum_case_convention rule with a new
    spectral-style enum-case-convention rule that will enforce
    the restriction that string-based enum values follow a
    specified case convention (default is snake case).

---
+++ packages/ruleset/test/enum-case-convention.test.js
@@ -0,0 +1,94 @@
+const { enumCaseConvention } = require('../src/rules');
+const { makeCopy, rootDocument, testRule, severityCodes } = require('./utils');
+
+const rule = enumCaseConvention;
+const ruleId = 'enum-case-convention';
+const expectedSeverity = severityCodes.error;
+const expectedMsg = 'Enum values must be snake case';
+
+describe('Spectral rule: enum-case-convention', () => {
+  describe('Should not yield errors', () => {
+    it('Clean spec', async () => {
+      const results = await testRule(ruleId, rule, rootDocument);
+      expect(results).toHaveLength(0);
+    });
+
+    it('Valid snake case enum value', async () => {
+      const testDocument = makeCopy(rootDocument);
+
+      testDocument.paths['/v1/movies'].get.parameters[0].schema.enum = [
+        'snake_case_value'
+      ];
+
+      const results = await testRule(ruleId, rule, testDocument);
+      expect(results).toHaveLength(0);
+    });
+  });
+
+  describe('Should yield errors', () => {
+    it('Parameter schema with non-snake case enum values', async () => {
+      const testDocument = makeCopy(rootDocument);
+
+      testDocument.paths['/v1/movies'].get.parameters[0].schema.enum = [
+        'valueOne',
+        'valueTwo',
+        'value_three'
+      ];
+
+      const results = await testRule(ruleId, rule, testDocument);
+      expect(results).toHaveLength(2);
+
+      for (const result of results) {
+        expect(result.code).toBe(ruleId);
+        expect(result.message).toBe(expectedMsg);
+        expect(result.severity).toBe(expectedSeverity);
+      }
+      expect(results[0].path.join('.')).toBe(
+        'paths./v1/movies.get.parameters.0.schema.enum.0'
+      );
+      expect(results[1].path.join('.')).toBe(
+        'paths./v1/movies.get.parameters.0.schema.enum.1'
+      );
+    });
+
+    it('Schema property with non-snake case enum values', async () => {
+      const testDocument = makeCopy(rootDocument);
+
+      testDocument.components.schemas['Juice'].properties['type'].enum = [
+        'juicyJuice',
+        'not_enough_juice',
+        'orangeJuice'
+      ];
+
+      const results = await testRule(ruleId, rule, testDocument);
+
+      // We should receive 2 errors because the Juice schema is used within the Drink schema,
+      // which is referenced in two places.
+      expect(results).toHaveLength(6);
+
+      for (const result of results) {
+        expect(result.code).toBe(ruleId);
+        expect(result.message).toBe(expectedMsg);
+        expect(result.severity).toBe(expectedSeverity);
+      }
+      expect(results[0].path.join('.')).toBe(
+        'paths./v1/drinks.post.requestBody.content.application/json.schema.oneOf.0.properties.type.enum.0'
+      );
+      expect(results[1].path.join('.')).toBe(
+        'paths./v1/drinks.post.requestBody.content.application/json.schema.oneOf.0.properties.type.enum.2'
+      );
+      expect(results[2].path.join('.')).toBe(
+        'paths./v1/drinks.post.responses.201.content.application/json.schema.oneOf.0.properties.type.enum.0'
+      );
+      expect(results[3].path.join('.')).toBe(
+        'paths./v1/drinks.post.responses.201.content.application/json.schema.oneOf.0.properties.type.enum.2'
+      );
+      expect(results[4].path.join('.')).toBe(
+        'paths./v1/drinks/{drink_id}.get.responses.200.content.application/json.schema.oneOf.0.properties.type.enum.0'
+      );
+      expect(results[5].path.join('.')).toBe(
+        'paths./v1/drinks/{drink_id}.get.responses.200.content.application/json.schema.oneOf.0.properties.type.enum.2'
+      );
+    });
+  });
+});
