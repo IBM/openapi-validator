950c650d572d6f614eef81b84d06db270e3250e7
Phil Adams <phil_adams@us.ibm.com>
2022-03-24 16:22:41 -0500

    fix(major-version-in-path): tolerate invalid server.url value

--- packages/ruleset/src/functions/check-major-version.js
+++ packages/ruleset/src/functions/check-major-version.js
@@ -14,9 +14,27 @@ module.exports = targetVal => {
   if (oas3) {
     const servers = targetVal['servers'];
     if (servers && Array.isArray(servers)) {
-      const urls = servers.map(o => o['url']);
-      const versions = urls.map(url => getVersion(url));
+      // Gather up all the unique path segments that
+      // indicate the API major version (e.g. /v1) within
+      // the URLs defined in the servers list.
+      const versions = [];
+      for (let i = 0; i < servers.length; i++) {
+        // Obtain the URL string from the server entry by resolving any
+        // variable references with each variable's default value.
+        const url = getDefaultUrl(servers[i]);
+        if (url) {
+          try {
+            const version = getVersion(url);
+            if (version) {
+              versions.push(version);
+            }
+          } catch (error) {
+            // Ignore any exceptions while parsing the URL string.
+          }
+        }
+      }
 
+      // If we have more than one unique "version" segment, then flag it.
       if (versions.length > 1 && !versions.every(v => v === versions[0])) {
         const uniqueVersions = versions.filter(
           (val, i, self) => self.indexOf(val) === i
@@ -88,9 +106,41 @@ module.exports = targetVal => {
   }
 };
 
-// Return the first segment of a path that matches the pattern 'v\d+'
-function getVersion(path) {
-  const url = new URL(path, 'https://foo.bar');
+/**
+ * Returns the first segment of the path portion of "urlString" that matches the pattern 'v\d+'
+ * @param {*} urlString an absolute or relative URL string (e.g. "https://myhost.com/api/v1" or "/v1/clouds").
+ * @returns the first path segment that appears to indicate the API major version (e.g. "v1").
+ */
+function getVersion(urlString) {
+  const url = new URL(urlString, 'https://foo.bar');
   const segments = url.pathname.split('/');
   return segments.find(segment => segment.match(/v[0-9]+/));
+}
+
+/**
+ * Returns the "default" server URL for the specified server object.
+ *
+ * @param {object} server an entry from the API definition's "servers" list.
+ * @returns the default URL string which is the result of replacing server
+ * variable references found in the server object's "url" field with
+ * the default value for each referenced server variable.
+ */
+function getDefaultUrl(server) {
+  let urlString = null;
+  if (server && server.url) {
+    urlString = server.url;
+
+    if (server.variables) {
+      for (const variable of Object.entries(server.variables)) {
+        const varName = variable[0];
+        const varObj = variable[1];
+        if (varName && varObj && varObj.default) {
+          const varRefRE = new RegExp('{' + varName + '}', 'g');
+          urlString = urlString.replace(varRefRE, varObj.default);
+        }
+      }
+    }
+  }
+
+  return urlString;
 }
