dc83bc61ab79b655d59f55a7925764be2b239143
Phil Adams <phil_adams@us.ibm.com>
2022-04-11 10:50:17 -0500

    fix(string-boundary): update string-boundary rule with new formats
    This commit updates the string-boundary rule slightly to be a bit
    more accurate in terms of the warnings that are issued for
    string properties with various format values specified for them.

--- packages/ruleset/src/functions/string-boundary.js
+++ packages/ruleset/src/functions/string-boundary.js
@@ -4,32 +4,59 @@ module.exports = function(schema, _opts, { path }) {
   return validateSubschemas(schema, path, stringBoundaryErrors);
 };
 
-function stringBoundaryErrors(schema, path) {
-  const errors = [];
-  if (schema.type !== 'string') {
-    return errors;
+// Rudimentary debug logging that is useful in debugging this rule.
+const debugEnabled = false;
+function debug(msg) {
+  if (debugEnabled) {
+    console.log(msg);
   }
+}
+
+// An object holding a list of "format" values to be bypassed when checking
+// for the "pattern", "minLength" and "maxLength" fields of a string property, respectively.
+const bypassFormats = {
+  pattern: ['binary', 'byte', 'date', 'date-time', 'url'],
+  minLength: ['date', 'identifier', 'url'],
+  maxLength: ['date']
+};
+
+function stringBoundaryErrors(schema, path) {
+  // We're only interested in checking string properties.
+  if (schema.type !== 'string') {
+    return [];
+  }
+
+  const errors = [];
   if (isUndefinedOrNull(schema.enum)) {
     if (
       isUndefinedOrNull(schema.pattern) &&
-      !['binary', 'date', 'date-time'].includes(schema.format)
+      !bypassFormats.pattern.includes(schema.format)
     ) {
       errors.push({
         message: 'Should define a pattern for a valid string',
         path
       });
+      debug('>>> pattern field missing for: ' + path.join('.'));
     }
-    if (isUndefinedOrNull(schema.minLength)) {
+    if (
+      isUndefinedOrNull(schema.minLength) &&
+      !bypassFormats.minLength.includes(schema.format)
+    ) {
       errors.push({
         message: 'Should define a minLength for a valid string',
         path
       });
+      debug('>>> minLength field missing for: ' + path.join('.'));
     }
-    if (isUndefinedOrNull(schema.maxLength)) {
+    if (
+      isUndefinedOrNull(schema.maxLength) &&
+      !bypassFormats.maxLength.includes(schema.format)
+    ) {
       errors.push({
         message: 'Should define a maxLength for a valid string',
         path
       });
+      debug('>>> maxLength field missing for: ' + path.join('.'));
     }
     if (
       !isUndefinedOrNull(schema.minLength) &&
@@ -40,6 +67,7 @@ function stringBoundaryErrors(schema, path) {
         message: 'minLength must be less than maxLength',
         path
       });
+      debug('>>> minLength >= maxLength for: ' + path.join('.'));
     }
   }
   return errors;
