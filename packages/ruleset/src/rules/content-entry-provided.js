8e008c25882049c3b0137c0097c8389221c95e66
Dustin Popp <dpopp07@gmail.com>
2022-04-18 10:16:52 -0500

    fix(content-entry-provided): dont require content entry for 304 responses
    The IBM API Handbook explicitly states that responses with a 304 status
    code MUST NOT contain a response body.

--- packages/ruleset/src/rules/content-entry-provided.js
+++ packages/ruleset/src/rules/content-entry-provided.js
@@ -5,7 +5,7 @@ module.exports = {
   description:
     'Request bodies and non-204 responses should define a content object',
   given: [
-    "$.paths[*][*].responses[?(@property != '204' && @property != '202' && @property != '101')]",
+    "$.paths[*][*].responses[?(@property != '204' && @property != '202' && @property != '101' && @property != '304')]",
     '$.paths[*][*].requestBody'
   ],
   severity: 'warn',
