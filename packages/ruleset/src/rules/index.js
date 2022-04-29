a56356c78ace83f234817312d064f39a15069369
Dan Hudlow <dhudlow@us.ibm.com>
2022-04-29 13:24:18 -0500

    refactor: simplify ruleset index files (#440)

--- packages/ruleset/src/rules/index.js
+++ packages/ruleset/src/rules/index.js
@@ -1,95 +1,48 @@
-const acceptParameter = require('./accept-parameter');
-const arrayOfArrays = require('./array-of-arrays');
-const arrayResponses = require('./array-responses');
-const authorizationParameter = require('./authorization-parameter');
-const binarySchemas = require('./binary-schemas');
-const contentEntryContainsSchema = require('./content-entry-contains-schema');
-const contentEntryProvided = require('./content-entry-provided');
-const contentTypeParameter = require('./content-type-parameter');
-const descriptionMentionsJSON = require('./description-mentions-json');
-const discriminator = require('./discriminator');
-const duplicatePathParameter = require('./duplicate-path-parameter');
-const enumCaseConvention = require('./enum-case-convention');
-const examplesNameContainsSpace = require('./examples-name-contains-space');
-const ibmContentTypeIsSpecific = require('./ibm-content-type-is-specific');
-const ibmErrorContentTypeIsJson = require('./ibm-error-content-type-is-json');
-const ibmSdkOperations = require('./ibm-sdk-operations');
-const inlineResponseSchema = require('./inline-response-schema');
-const majorVersionInPath = require('./major-version-in-path');
-const missingRequiredProperty = require('./missing-required-property');
-const operationIdCaseConvention = require('./operation-id-case-convention');
-const operationIdNamingConvention = require('./operation-id-naming-convention');
-const operationSummary = require('./operation-summary');
-const paginationStyle = require('./pagination-style');
-const parameterCaseConvention = require('./parameter-case-convention');
-const parameterDefault = require('./parameter-default');
-const parameterDescription = require('./parameter-description');
-const parameterOrder = require('./parameter-order');
-const parameterSchemaOrContent = require('./parameter-schema-or-content');
-const pathSegmentCaseConvention = require('./path-segment-case-convention');
-const prohibitSummarySentenceStyle = require('./prohibit-summary-sentence-style');
-const propertyCaseCollision = require('./property-case-collision');
-const propertyCaseConvention = require('./property-case-convention');
-const propertyDescription = require('./property-description');
-const propertyInconsistentNameAndType = require('./property-inconsistent-name-and-type');
-const refPattern = require('./ref-pattern');
-const requestBodyName = require('./request-body-name');
-const requestBodyObject = require('./request-body-object');
-const responseErrorResponseSchema = require('./response-error-response-schema');
-const responseExampleProvided = require('./response-example-provided');
-const responseStatusCodes = require('./response-status-codes');
-const schemaDescription = require('./schema-description');
-const securitySchemes = require('./security-schemes');
-const serverVariableDefaultValue = require('./server-variable-default-value');
-const stringBoundary = require('./string-boundary');
-const unusedTag = require('./unused-tag');
-const validTypeFormat = require('./valid-type-format');
-
 module.exports = {
-  acceptParameter,
-  arrayOfArrays,
-  arrayResponses,
-  binarySchemas,
-  authorizationParameter,
-  contentEntryContainsSchema,
-  contentEntryProvided,
-  contentTypeParameter,
-  descriptionMentionsJSON,
-  discriminator,
-  duplicatePathParameter,
-  enumCaseConvention,
-  examplesNameContainsSpace,
-  ibmContentTypeIsSpecific,
-  ibmErrorContentTypeIsJson,
-  ibmSdkOperations,
-  inlineResponseSchema,
-  majorVersionInPath,
-  missingRequiredProperty,
-  operationIdCaseConvention,
-  operationIdNamingConvention,
-  operationSummary,
-  paginationStyle,
-  parameterCaseConvention,
-  parameterDefault,
-  parameterDescription,
-  parameterOrder,
-  parameterSchemaOrContent,
-  pathSegmentCaseConvention,
-  prohibitSummarySentenceStyle,
-  propertyCaseCollision,
-  propertyCaseConvention,
-  propertyDescription,
-  propertyInconsistentNameAndType,
-  refPattern,
-  requestBodyName,
-  requestBodyObject,
-  responseErrorResponseSchema,
-  responseExampleProvided,
-  responseStatusCodes,
-  schemaDescription,
-  securitySchemes,
-  serverVariableDefaultValue,
-  stringBoundary,
-  unusedTag,
-  validTypeFormat
+  acceptParameter: require('./accept-parameter'),
+  arrayOfArrays: require('./array-of-arrays'),
+  arrayResponses: require('./array-responses'),
+  authorizationParameter: require('./authorization-parameter'),
+  binarySchemas: require('./binary-schemas'),
+  contentEntryContainsSchema: require('./content-entry-contains-schema'),
+  contentEntryProvided: require('./content-entry-provided'),
+  contentTypeParameter: require('./content-type-parameter'),
+  descriptionMentionsJSON: require('./description-mentions-json'),
+  discriminator: require('./discriminator'),
+  duplicatePathParameter: require('./duplicate-path-parameter'),
+  enumCaseConvention: require('./enum-case-convention'),
+  examplesNameContainsSpace: require('./examples-name-contains-space'),
+  ibmContentTypeIsSpecific: require('./ibm-content-type-is-specific'),
+  ibmErrorContentTypeIsJson: require('./ibm-error-content-type-is-json'),
+  ibmSdkOperations: require('./ibm-sdk-operations'),
+  inlineResponseSchema: require('./inline-response-schema'),
+  majorVersionInPath: require('./major-version-in-path'),
+  missingRequiredProperty: require('./missing-required-property'),
+  operationIdCaseConvention: require('./operation-id-case-convention'),
+  operationIdNamingConvention: require('./operation-id-naming-convention'),
+  operationSummary: require('./operation-summary'),
+  paginationStyle: require('./pagination-style'),
+  parameterCaseConvention: require('./parameter-case-convention'),
+  parameterDefault: require('./parameter-default'),
+  parameterDescription: require('./parameter-description'),
+  parameterOrder: require('./parameter-order'),
+  parameterSchemaOrContent: require('./parameter-schema-or-content'),
+  pathSegmentCaseConvention: require('./path-segment-case-convention'),
+  prohibitSummarySentenceStyle: require('./prohibit-summary-sentence-style'),
+  propertyCaseCollision: require('./property-case-collision'),
+  propertyCaseConvention: require('./property-case-convention'),
+  propertyDescription: require('./property-description'),
+  propertyInconsistentNameAndType: require('./property-inconsistent-name-and-type'),
+  refPattern: require('./ref-pattern'),
+  requestBodyName: require('./request-body-name'),
+  requestBodyObject: require('./request-body-object'),
+  responseErrorResponseSchema: require('./response-error-response-schema'),
+  responseExampleProvided: require('./response-example-provided'),
+  responseStatusCodes: require('./response-status-codes'),
+  schemaDescription: require('./schema-description'),
+  securitySchemes: require('./security-schemes'),
+  serverVariableDefaultValue: require('./server-variable-default-value'),
+  stringBoundary: require('./string-boundary'),
+  unusedTag: require('./unused-tag'),
+  validTypeFormat: require('./valid-type-format')
 };
--- packages/ruleset/src/utils/index.js
+++ packages/ruleset/src/utils/index.js
@@ -1,71 +1,19 @@
-const checkCompositeSchemaForConstraint = require('./check-composite-schema-for-constraint');
-const checkCompositeSchemaForProperty = require('./check-composite-schema-for-property');
-const getPropertyNamesForSchema = require('./get-property-names-for-schema');
-const isDeprecated = require('./is-deprecated');
-const isFormMimeType = require('./is-form-mimetype');
-const isJsonMimeType = require('./is-json-mimetype');
-const isObject = require('./is-object');
-const isPrimitiveType = require('./is-primitive-type');
-const isSdkExcluded = require('./is-sdk-excluded');
-const mergeAllOfSchemaProperties = require('./merge-allof-schema-properties');
-const operationMethods = require('./constants');
-const pathMatchesRegexp = require('./path-matches-regexp');
-const schemaRequiresProperty = require('./schema-requires-property');
-const validateComposedSchemas = require('./validate-composed-schemas');
-const validateNestedSchemas = require('./validate-nested-schemas');
-const validateSubschemas = require('./validate-subschemas');
-const {
-  SchemaType,
-  getSchemaType,
-  isArraySchema,
-  isBinarySchema,
-  isByteSchema,
-  isBooleanSchema,
-  isDateSchema,
-  isDateTimeSchema,
-  isDoubleSchema,
-  isEnumerationSchema,
-  isFloatSchema,
-  isInt32Schema,
-  isInt64Schema,
-  isIntegerSchema,
-  isNumberSchema,
-  isObjectSchema,
-  isStringSchema
-} = require('./get-schema-type');
-
 module.exports = {
-  checkCompositeSchemaForConstraint,
-  checkCompositeSchemaForProperty,
-  getPropertyNamesForSchema,
-  getSchemaType,
-  isArraySchema,
-  isBinarySchema,
-  isByteSchema,
-  isBooleanSchema,
-  isDateSchema,
-  isDateTimeSchema,
-  isDeprecated,
-  isDoubleSchema,
-  isEnumerationSchema,
-  isFloatSchema,
-  isFormMimeType,
-  isInt32Schema,
-  isInt64Schema,
-  isIntegerSchema,
-  isJsonMimeType,
-  isNumberSchema,
-  isObject,
-  isObjectSchema,
-  isPrimitiveType,
-  isSdkExcluded,
-  isStringSchema,
-  mergeAllOfSchemaProperties,
-  operationMethods,
-  pathMatchesRegexp,
-  schemaRequiresProperty,
-  validateComposedSchemas,
-  validateNestedSchemas,
-  validateSubschemas,
-  SchemaType
+  checkCompositeSchemaForConstraint: require('./check-composite-schema-for-constraint'),
+  checkCompositeSchemaForProperty: require('./check-composite-schema-for-property'),
+  getPropertyNamesForSchema: require('./get-property-names-for-schema'),
+  isDeprecated: require('./is-deprecated'),
+  isFormMimeType: require('./is-form-mimetype'),
+  isJsonMimeType: require('./is-json-mimetype'),
+  isObject: require('./is-object'),
+  isPrimitiveType: require('./is-primitive-type'),
+  isSdkExcluded: require('./is-sdk-excluded'),
+  mergeAllOfSchemaProperties: require('./merge-allof-schema-properties'),
+  operationMethods: require('./constants'),
+  pathMatchesRegexp: require('./path-matches-regexp'),
+  schemaRequiresProperty: require('./schema-requires-property'),
+  validateComposedSchemas: require('./validate-composed-schemas'),
+  validateNestedSchemas: require('./validate-nested-schemas'),
+  validateSubschemas: require('./validate-subschemas'),
+  ...require('./get-schema-type')
 };
