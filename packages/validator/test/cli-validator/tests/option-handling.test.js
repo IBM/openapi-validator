e6520fb06ecc23d516c1844f6022b140a81e0f2b
Dustin Popp <dpopp07@gmail.com>
2022-07-22 11:20:07 -0500

    fix: update spectral version to resolve path mapping bug (#465)
    This resolves an issue in which users would see validations with
    paths that did not point to the correct location in the API definition.
    This was caused by a bug in Spectral that has now been fixed.
    
    Signed-off-by: Dustin Popp dustinpopp@ibm.com

--- packages/validator/test/cli-validator/tests/option-handling.test.js
+++ packages/validator/test/cli-validator/tests/option-handling.test.js
@@ -129,7 +129,7 @@ describe('cli tool - test option handling', function() {
 
     // totals
     expect(capturedText[statsSection + 1].match(/\S+/g)[5]).toEqual('2');
-    expect(capturedText[statsSection + 2].match(/\S+/g)[5]).toEqual('3');
+    expect(capturedText[statsSection + 2].match(/\S+/g)[5]).toEqual('4');
 
     // errors
     expect(capturedText[statsSection + 5].match(/\S+/g)[0]).toEqual('1');
@@ -140,10 +140,13 @@ describe('cli tool - test option handling', function() {
 
     // warnings
     expect(capturedText[statsSection + 9].match(/\S+/g)[0]).toEqual('1');
-    expect(capturedText[statsSection + 9].match(/\S+/g)[1]).toEqual('(33%)');
+    expect(capturedText[statsSection + 9].match(/\S+/g)[1]).toEqual('(25%)');
 
-    expect(capturedText[statsSection + 10].match(/\S+/g)[0]).toEqual('1');
-    expect(capturedText[statsSection + 10].match(/\S+/g)[1]).toEqual('(33%)');
+    expect(capturedText[statsSection + 10].match(/\S+/g)[0]).toEqual('2');
+    expect(capturedText[statsSection + 10].match(/\S+/g)[1]).toEqual('(50%)');
+
+    expect(capturedText[statsSection + 11].match(/\S+/g)[0]).toEqual('1');
+    expect(capturedText[statsSection + 11].match(/\S+/g)[1]).toEqual('(25%)');
   });
 
   it('should not print statistics report by default', async function() {
