c289a164f3a834f22bedd5d74b700d46f3bc5e6b
Phil Adams <phil_adams@us.ibm.com>
2022-12-07 12:22:27 -0600

    fix: create executables in correct package
    Signed-off-by: Phil Adams <phil_adams@us.ibm.com>

--- scripts/create-binaries.sh
+++ scripts/create-binaries.sh
@@ -1,6 +1,12 @@
-./node_modules/.bin/pkg --out-path=./bin ./packages/validator/package.json
+#!/bin/bash
 
+# This script will be run when "npm run pkg" is executed from within
+# the "packages/validator" directory.
+# The commands below assume that the current directory is packages/validator.
+../../node_modules/.bin/pkg --out-path=./bin ./package.json
+ 
 cd ./bin
 mv ibm-openapi-validator-macos lint-openapi-macos
 mv ibm-openapi-validator-linux lint-openapi-linux
 mv ibm-openapi-validator-win.exe lint-openapi-win.exe
+chmod +x lint-openapi-win.exe
