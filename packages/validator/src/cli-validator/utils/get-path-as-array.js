c88298e58abe722f2d7be0d024eb649b65601a3a
Dustin Popp <dpopp07@gmail.com>
2023-03-06 12:36:26 -0500

    refactor: optimize logic for summary-only feature (#565)
    Currently, we loop through all validations and check if "summary only" is enabled before
    we print them. It would be more efficient and readable to check once at the beginning,
    printing the summary and exiting if the feature is enabled.
    
    This commit also includes some other, minor refactoring.
    
    Signed-off-by: Dustin Popp <dpopp07@gmail.com>

--- packages/validator/src/cli-validator/utils/get-path-as-array.js
+++
@@ -1,8 +0,0 @@
-/**
- * Copyright 2017 - 2023 IBM Corporation.
- * SPDX-License-Identifier: Apache2.0
- */
-
-module.exports = function(path) {
-  return Array.isArray(path) ? path : path.split('.');
-};
