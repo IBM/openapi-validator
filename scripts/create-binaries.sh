7b8192dc74a4d672a8b6a6a077e40fc9e4ea716a
Phil Adams <phil_adams@us.ibm.com>
2024-03-22 10:57:35 -0500

    fix(build): build executables with correct dependency versions (#654)
    This commit modifies the create-binaries.sh script so that it
    removes the packages/validator/node_modules/@ibm-cloud and
    packages/ruleset/node_modules/@ibm-cloud directories
    prior to invoking pkg to build the executables.
    This fixes a problem where the executables would end up with
    the "old" versions of the openapi-ruleset and
    openapi-ruleset-utilities dependencies in situations
    where we're also publishing a new version of those dependencies
    in the same build.
    Removing the dependencies from packages/[validator,ruleset] means
    that pkg would end up using the dependencies in the project's
    top-level "node_modules" directory instead, which will have the correct
    versions of the dependencies, whether or not we've published a new
    version of them during the same build.
    
    Signed-off-by: Phil Adams <phil_adams@us.ibm.com>

--- scripts/create-binaries.sh
+++ scripts/create-binaries.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+# Enable shell debug mode so we get a few additional details in the build log.
+set -x
+
 # This script will be run when "npm run pkg" is executed from within
 # the "packages/validator" directory.
 # The commands below assume that the current directory is packages/validator.
+
+# Before creating the executables, we need to remove the "openapi-ruleset"
+# and "openapi-ruleset-utilities" dependencies from the packages/validator/node_modules
+# and packages/ruleset/node_modules directories (respectively).
+# We need to do this because those locations will actually contain the prior version 
+# of the dependency if we've published a new release of it during the same build.
+# By removing them from the packages/[validator,ruleset] directories, we ensure that
+# the correct version of these dependencies is obtained from the project's top-level
+# node_modules directory instead.
+if [[ -e "node_modules/@ibm-cloud" ]]; then
+    rm -fr "node_modules/@ibm-cloud"
+fi
+
+if [[ -e "../ruleset/node_modules/@ibm-cloud" ]]; then
+    rm -fr "../ruleset/node_modules/@ibm-cloud"
+fi
+
+# Create the executables
 ../../node_modules/.bin/pkg --out-path=./bin ./package.json
- 
+
+# Rename the executables and set their execute bit.
 cd ./bin
 mv ibm-openapi-validator-macos lint-openapi-macos
 mv ibm-openapi-validator-linux lint-openapi-linux
