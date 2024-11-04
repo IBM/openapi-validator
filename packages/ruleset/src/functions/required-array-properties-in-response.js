415fdcc71d23e99e93e6861f987e3de327a1146e
Phil Adams <phil_adams@us.ibm.com>
2024-11-04 16:00:50 -0600

    fix(ibm-required-array-properties-in-response): avoid false positives within composed models
    This commit improves the accuracy of the logic used in the 'ibm-required-array-properties-in-response'
    so that we no longer return errors for array properties defined inside of
    allOf/anyOf/oneOf lists where the required field might be specified in the containing schema.
    
    Signed-off-by: Phil Adams <phil_adams@us.ibm.com>

--- packages/ruleset/src/functions/required-array-properties-in-response.js
+++ packages/ruleset/src/functions/required-array-properties-in-response.js
@@ -6,7 +6,7 @@
 const {
   isArraySchema,
   isObject,
-  validateSubschemas,
+  validateNestedSchemas,
 } = require('@ibm-cloud/openapi-ruleset-utilities');
 const { LoggerFactory } = require('../utils');
 
@@ -19,36 +19,63 @@ module.exports = function (schema, _opts, context) {
     logger = LoggerFactory.getInstance().getLogger(ruleId);
   }
 
-  return validateSubschemas(
-    schema,
-    context.path,
-    checkForOptionalArrays,
-    true,
-    false
-  );
+  return validateNestedSchemas(schema, context.path, (s, p) => {
+    return checkForOptionalArrays(s, p, []);
+  });
 };
 
 /**
- * Checks "schema" for any optional array properties.
+ * Checks "schema" for any optional array properties, including
+ * properties defined within allOf/anyOf/oneOf lists reachable from "schema".
  * @param {*} schema the schema to check
- * @param {*} path the json path location of "schema"
- * @returns
+ * @param {*} path the jsonpath location of "schema"
+ * @returns an array of zero or more errors
  */
-function checkForOptionalArrays(schema, path) {
-  // If "schema" defines properties, then add an error for each optional array property.
-  if (isObject(schema) && isObject(schema.properties)) {
+function checkForOptionalArrays(schema, path, contextualRequired) {
+  if (isObject(schema)) {
+    logger.debug(`${ruleId}: examining schema at location: ${path.join('.')}`);
+
+    // The set of required property names that we'll use for our required/optional
+    // checks consists of "schema"'s required list plus the property names passed in via
+    // "contextualRequired".
+    const requiredProps = [];
+    if (Array.isArray(schema.required)) {
+      requiredProps.push(...schema.required);
+    }
+    requiredProps.push(...contextualRequired);
+
     logger.debug(
-      `${ruleId}: examining object schema at location: ${path.join('.')}`
+      `${ruleId}: required properties: ${JSON.stringify(requiredProps)}`
     );
 
     const errors = [];
 
-    const requiredProps = schema.required || [];
-    for (const [name, prop] of Object.entries(schema.properties)) {
-      if (isArraySchema(prop) && !requiredProps.includes(name)) {
-        errors.push({
-          message: 'In a response body, an array field MUST NOT be optional',
-          path: [...path, 'properties', name],
+    // 1. Check the properties defined directly in "schema".
+    if (isObject(schema.properties)) {
+      for (const [name, prop] of Object.entries(schema.properties)) {
+        logger.debug(`${ruleId}: examining property '${name}')}`);
+
+        if (isArraySchema(prop) && !requiredProps.includes(name)) {
+          logger.debug(`${ruleId}: property '${name}' failed validation!`);
+          errors.push({
+            message: 'In a response body, an array field MUST NOT be optional',
+            path: [...path, 'properties', name],
+          });
+        }
+      }
+    }
+
+    // 2. Recursively check each allOf/anyOf/oneOf list element schemas.
+    for (const applicatorType of ['allOf', 'anyOf', 'oneOf']) {
+      if (Array.isArray(schema[applicatorType])) {
+        schema[applicatorType].forEach((s, i) => {
+          errors.push(
+            ...checkForOptionalArrays(
+              s,
+              [...path, applicatorType, i.toString()],
+              requiredProps
+            )
+          );
         });
       }
     }
