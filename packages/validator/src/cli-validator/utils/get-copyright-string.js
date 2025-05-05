0d4d8badae69b35246ba2730a2981664ac2e43fa
Phil Adams <phil_adams@us.ibm.com>
2025-05-05 10:53:16 -0500

    fix: update year in copyright statement (#745)
    Signed-off-by: Phil Adams <phil_adams@us.ibm.com>

--- packages/validator/src/cli-validator/utils/get-copyright-string.js
+++ packages/validator/src/cli-validator/utils/get-copyright-string.js
@@ -1,10 +1,10 @@
 /**
- * Copyright 2017 - 2023 IBM Corporation.
+ * Copyright 2017 - 2025 IBM Corporation.
  * SPDX-License-Identifier: Apache2.0
  */
 
 const getVersionString = require('./get-version-string');
 
 module.exports = function () {
-  return `IBM OpenAPI Validator (${getVersionString()}), @Copyright IBM Corporation 2017, 2024.`;
+  return `IBM OpenAPI Validator (${getVersionString()}), @Copyright IBM Corporation 2017, ${new Date().getFullYear()}.`;
 };
